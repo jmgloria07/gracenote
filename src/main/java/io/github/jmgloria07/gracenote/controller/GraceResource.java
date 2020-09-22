57589b35e035aa8b8af5f5ed4ac74d8ba220b5ce
jmgloria07 <joshua.gloria07@gmail.com>
2020-09-22 16:27:38 +0800

    v0.3.2 Update method URLs of Grace resource
    > Prepend /users/ to URL
    > Increment patch version

--- src/main/java/io/github/jmgloria07/gracenote/controller/GraceResource.java
+++ src/main/java/io/github/jmgloria07/gracenote/controller/GraceResource.java
@@ -21,17 +21,17 @@ public class GraceResource {
 	@Autowired
 	GraceService graceService;
 	
-	@GetMapping(path = "/{user}/graces/{id}") 
+	@GetMapping(path = "/users/{user}/graces/{id}") 
 	public Grace getGrace(@PathVariable long user, @PathVariable long id) {
 		return graceService.getGrace(user, id);
 	}
 	
-	@GetMapping(path = "/{user}/graces") 
+	@GetMapping(path = "/users/{user}/graces") 
 	public List<Grace> getGrace(@PathVariable long user) {
 		return graceService.getGrace(user);
 	}
 	
-	@PostMapping(path = "/{user}/graces")
+	@PostMapping(path = "/users/{user}/graces")
 	public Grace postGrace(@PathVariable long user, 
 			@RequestBody GraceForm graceForm) {
 		return graceService.postGrace(user, graceForm);
